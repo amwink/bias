1937c1c66aa8df3b5b1ea93f63db6af8cdf26606
Alle Meije Wink <amwink@amwink-rad-vumc>
2013-10-23 15:15:00 +0200

    code review and reformatting

--- matlab/fastECM/quantiles.m
+++ matlab/fastECM/quantiles.m
@@ -1,4 +1,4 @@
-function  q = quantile(x,p,method)
+function  q = quantiles(x,p,method)
 %QUANTILE Empirical quantile (percentile).
 %
 %         q = quantile(x,p)
@@ -18,39 +18,38 @@ function  q = quantile(x,p,method)
 
 if nargin<3, method=1; end
 if min(size(x)) == 1
-   x = x(:);
-   q = zeros(size(p));
+  x = x(:);
+  q = zeros(size(p));
 else
-   if min(size(p)) > 1 
-      error('Not both matrix x and matrix p input')
-   end
-   q = zeros(length(p),size(x,2));
+  if min(size(p)) > 1 
+    error('Not both matrix x and matrix p input')
+  end
+  q = zeros(length(p),size(x,2));
 end
 if any(any((p>1|p<0)))
-   error('Input p is not probability')
+  error('Input p is not probability')
 end
 
 x = sort(x); 
 p = p(:);
 n = size(x,1);
 if method == 3
-   qq1 = x(ceil(max(1,p*n)),:); 
-   qq2 = x(floor(min(p*n+1,n)),:);
-   qq = (qq1+qq2)/2;
+  qq1 = x(ceil(max(1,p*n)),:); 
+  qq2 = x(floor(min(p*n+1,n)),:);
+  qq = (qq1+qq2)/2;
 else                         
-   x = [x(1,:); x; x(n,:)];
-   if method == 2
-      % This method is from Hjort's "Computer
-      % intensive statistical methods" page 102
-      i = p*(n+1)+1;
-   else % Metod 1
-      i = p*n+1.5;
-   end
-   iu = ceil(i);
-   il = floor(i);
-   d = (i-il)*ones(1,size(x,2));
-   qq = x(il,:).*(1-d)+x(iu,:).*d;
+  x = [x(1,:); x; x(n,:)];
+  if method == 2
+    % This method is from Hjort's "Computer
+    % intensive statistical methods" page 102
+    i = p*(n+1)+1;
+  else % Metod 1
+    i = p*n+1.5;
+  end
+  iu = ceil(i);
+  il = floor(i);
+  d = (i-il)*ones(1,size(x,2));
+  qq = x(il,:).*(1-d)+x(iu,:).*d;
 end
 
 q(:) = qq;
-
